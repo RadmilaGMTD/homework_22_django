e5304e7ee9b0de3ff0f16f7f2c3d1f5bced2fc82
Radmila Gimatdinova <raf1507@mail.ru>
2025-01-31 02:09:56 +0400

    create migrate

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Наименование категории")
+    description = models.TextField(verbose_name="Описание категории")
+
+    def __str__(self):
+        return {self.name}
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Наименование продукта")
+    description = models.TextField(verbose_name="Описание продукта")
+    image = models.ImageField(upload_to="catalog/image", null=True, blank=True, verbose_name="Изображение продукта")
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, verbose_name="Категория продукта", null=True, blank=True)
+    price = models.CharField(max_length=50, verbose_name="Цена за покупку")
+    created_at = models.DateField(verbose_name="Дата создания")
+    updated_at = models.DateField(verbose_name="Дата последнего изменения")
+
+    def __str__(self):
+        return f"{self.name} {self.price}"
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ["name", "category"]
+
