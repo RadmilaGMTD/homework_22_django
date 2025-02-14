32ecacbd4d4326c50a51f2ba3f0ee3065baa37cd
Radmila Gimatdinova <raf1507@mail.ru>
2025-02-15 03:41:46 +0400

    update app blog with CRUD

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, View
+from .models import Blog
+from django.urls import reverse_lazy, reverse
 
-# Create your views here.
+
+class BlogListView(ListView):
+    model = Blog
+
+class BlogDetailView(DetailView):
+    model = Blog
+
+class BlogCreateView(CreateView):
+    model = Blog
+    fields = ['title', 'content', 'image']
+    success_url = reverse_lazy('blog:blog_list')
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ['title', 'content', 'image']
+    def get_success_url(self):
+        return reverse('blog:blog_detail', kwargs={'pk': self.object.pk})
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    success_url = reverse_lazy('blog:blog_list')
