c032595046c4af0b5e6d484eed2c6b60b0e76d37
Radmila Gimatdinova <raf1507@mail.ru>
2025-02-15 04:39:38 +0400

    update views.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from . import views
 from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
 
 app_name = CatalogConfig.name
--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,46 @@
-from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, View
 from .models import Blog
 from django.urls import reverse_lazy, reverse
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 class BlogListView(ListView):
     model = Blog
+    def get_queryset(self):
+        return Blog.objects.filter(is_publication=True)
+
 
 class BlogDetailView(DetailView):
     model = Blog
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        obj.views += 1
+        obj.save(update_fields=['views'])
+
+        if obj.views == 100:
+            send_mail(
+                'Поздравляем!',
+                f'Ваша статья "{obj.title}" достигла 100 просмотров!',
+                settings.DEFAULT_FROM_EMAIL,
+                ['rmiftyaeva@mail.ru'],
+                fail_silently=False,
+            )
+        return obj
+
 
 class BlogCreateView(CreateView):
     model = Blog
-    fields = ['title', 'content', 'image']
+    fields = ['title', 'content', 'image', 'is_publication']
     success_url = reverse_lazy('blog:blog_list')
+
 
 class BlogUpdateView(UpdateView):
     model = Blog
-    fields = ['title', 'content', 'image']
+    fields = ['title', 'content', 'image', 'is_publication']
     def get_success_url(self):
         return reverse('blog:blog_detail', kwargs={'pk': self.object.pk})
+
 
 class BlogDeleteView(DeleteView):
     model = Blog
